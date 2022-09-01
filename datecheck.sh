ae8ceae4c6e45bd99bd42a1854851ed5af408bd9
Simon Saberian <simon.saberian@gmail.com>
2022-09-01 17:21:20 +0200

    wow

--- datecheck.sh
+++ datecheck.sh
@@ -10,27 +10,83 @@ if [[ $DAY -lt 1 ]]; then
 	exit
 fi
 
-if (($YEAR % 4 == 0)); then
-	if (($YEAR % 100 == 0)); then
-		if (($YEAR % 400 == 0)); then
+if [[ $(expr $YEAR % 4) == 0 ]]; then
+	if [[ $(expr $YEAR % 100) == 0 ]]; then
+		if [[ $(expr $YEAR % 400) == 0 ]]; then
+			echo "here 1"
 			LEAP=True
 		else
+			echo "here 2"
 			LEAP=False
 		fi
 	else
+		echo "here 3"
 		LEAP=True
 	fi
 else
+	echo "here 4"
 	LEAP=False
 fi
 
 case $MONTH in
-1 | [Jj]an) echo "wow its january" ;;
-2 | [Ff]eb) echo "wow its january" ;;
-3 | [Mm]ar) echo "wow its january" ;;
-4 | [Aa]pr) echo "wow its january" ;;
-5 | [Mm]ay) echo "wow its january" ;;
-6 | [Jj]un) echo "wow its january" ;;
+1 | [Jj]an)
+	if [[ $DAY -gt 31 ]]; then
+		echo "Dec does not have $DAY days"
+		exit
+	else
+		MONTH="Dec"
+	fi
+	;;
+2 | [Ff]eb)
+	if [ $LEAP == True ]; then
+		echo "is leap"
+		if [[ $DAY -gt 29 ]]; then
+			echo "Dec does not have $DAY days"
+			exit
+		else
+			MONTH="Dec"
+		fi
+	else
+		if [[ $DAY -gt 28 ]]; then
+			echo "Dec does not have $DAY days"
+			exit
+		else
+			MONTH="Dec"
+		fi
+	fi
+	;;
+3 | [Mm]ar)
+	if [[ $DAY -gt 31 ]]; then
+		echo "Dec does not have $DAY days"
+		exit
+	else
+		MONTH="Dec"
+	fi
+	;;
+4 | [Aa]pr)
+	if [[ $DAY -gt 31 ]]; then
+		echo "Dec does not have $DAY days"
+		exit
+	else
+		MONTH="Dec"
+	fi
+	;;
+5 | [Mm]ay)
+	if [[ $DAY -gt 31 ]]; then
+		echo "Dec does not have $DAY days"
+		exit
+	else
+		MONTH="Dec"
+	fi
+	;;
+6 | [Jj]un)
+	if [[ $DAY -gt 31 ]]; then
+		echo "Dec does not have $DAY days"
+		exit
+	else
+		MONTH="Dec"
+	fi
+	;;
 7 | [Jj]ul)
 	if [[ $DAY -gt 31 ]]; then
 		echo "Jul does not have $DAY days"
