26ad0d8827a451d2b30801c4b24ee55ae1fd4f6f
Simon Saberian <simon.saberian@gmail.com>
2022-10-03 16:02:28 +0200

    server that listens to multiple clients with fork

--- assignment2/mathserver/src/server.c
+++ assignment2/mathserver/src/server.c
@@ -1,5 +1,48 @@
-#include "stdio.h"
+#include <netinet/in.h> //structure for storing address information
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> //for socket APIs
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
+  // create file descriptor
+  int sfd = socket(AF_INET,SOCK_STREAM,0);
+
+  // define server address and port
+  struct sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(9001);
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+  bind(sfd, &serverAddr, sizeof(serverAddr));
+  listen(sfd, 1);
+
+  printf("Listening..\n");
+
+  for(;;){
+
+    int conn = accept(sfd, NULL, NULL);
+
+    int pid = fork();
+
+    if (pid == -1)
+    {
+      printf("Could not create new process\n");
+      exit(-1);
+    }
+    
+    if (pid == 0)
+    {
+      // child fork
+      char msg[255] = "hejhejhej";
+      send(conn, msg, sizeof(msg),0);
+
+      exit(0);
+    }
+
+    signal(SIGCHLD,SIG_IGN);
+  }
+
   printf("hello\n");  
 }
